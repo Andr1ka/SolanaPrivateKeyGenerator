62b391de9f1f564401cd7fa144f8e75c13f9a0d8
Andrei <kuznechikovandrei@mail.ru>
2025-01-21 06:13:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Solnet.Wallet;
+using Solnet.Wallet.Bip39;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("count of private keys ");
+        if (int.TryParse(Console.ReadLine(), out int numberOfKeys) && numberOfKeys > 0)
+        {
+            for (int i = 0; i < numberOfKeys; i++)
+            {
+                // Генерируем новую мнемоническую фразу
+                var newMnemonic = new Mnemonic(WordList.English, WordCount.Twelve);
+
+                // Инициализируем кошелек с использованием сгенерированной мнемонической фразы
+                var wallet = new Wallet(newMnemonic);
+
+                // Получаем приватный ключ
+                var privateKey = wallet.Account.PrivateKey;
+
+                Console.WriteLine(privateKey);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректное количество. Пожалуйста, введите положительное целое число.");
+        }
+    }
+}
